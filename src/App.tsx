ed088d5264f87baae599b3bf5c334e806cd4bc81
Jonathan Narvaez <narvaez.jonathan18@gmail.com>
2024-04-11 22:46:52 +0800

    Feat: message arrays, back and next button for switching messages, animation for message container and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,51 @@
+import { useState } from "react";
 import uwugif from "./assets/erm-fingers.gif";
 
+let messageArray = [
+  "huy! kumusta na hahaha, hope ur doin well",
+  "may itatanong me hahaha, mukhang long shot but anyways...",
+  "if free ka this May, aayain sana kita, for a second date :)",
+];
+
 function App() {
+  const [messageToDisplay, setMessageToDisplay] = useState(0);
   return (
     <>
       <main className="bg-salmon min-h-svh flex flex-col items-center justify-center gap-5">
         <section className="flex flex-col gap-5">
-          <div className="bg-purple p-5 rounded-lg shadow-lg max-w-90 m-auto">
-            <p className="text-2xl text-cream-dark font-medium">
-              hey! blah blah blah blah blah blah blah,
+          <div className="bg-purple p-5 rounded-lg shadow-lg max-w-90 m-auto animate-ball">
+            <p className="text-2xl text-cream-dark font-medium text-center animate-fade">
+              {messageArray[messageToDisplay]}
             </p>
           </div>
           <div className="flex justify-center gap-5">
             <button
-              className={
-                "bg-cream-light py-3 w-24 rounded-md shadow-xl text-purple text-lg font-bold"
-              }
+              className={`bg-cream-light py-3 w-24 rounded-md shadow-xl text-purple text-lg font-bold ${
+                messageToDisplay == 0 ? "opacity-25" : ""
+              }`}
+              onClick={() => {
+                if (messageToDisplay == messageArray.length - 1) {
+                  //
+                } else
+                  setMessageToDisplay((prev) => (prev > 0 ? prev - 1 : prev));
+              }}
             >
-              Yes
+              {messageToDisplay == messageArray.length - 1 ? "No" : "Back"}
             </button>
             <button
               className={
                 "bg-cream-light py-3 w-24 rounded-md shadow-xl text-purple text-lg font-bold"
               }
+              onClick={() => {
+                if (messageToDisplay == messageArray.length - 1) {
+                  //
+                } else
+                  setMessageToDisplay((prev) =>
+                    prev == messageArray.length - 1 ? prev : prev + 1
+                  );
+              }}
             >
-              No
+              {messageToDisplay == messageArray.length - 1 ? "Yes" : "Next"}
             </button>
           </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { transform } from "typescript";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -11,6 +13,20 @@ export default {
       },
       spacing: {
         90: "90%",
+      },
+      animation: {
+        fade: "fade 0.5s ease",
+        ball: "ball 0.2s ease",
+      },
+      keyframes: {
+        fade: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        ball: {
+          "0%": { transform: "translateY(10px)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
     },
   },
